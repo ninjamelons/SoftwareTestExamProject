da251cf3c58ec70f0109729a1f437f4d7840b32a
Nohbdys <Billen93@outlook.com>
2020-05-27 14:17:27 +0200

    Forgot new classes in commit 3

---
+++ SoftwareTestExamProject/Functionality/Enemy.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftwareTestExamProject.Functionality
+{
+    public class Enemy : Character
+    {
+        private bool isDefending = false;
+        public bool IsDefending { get => isDefending; }
+
+        public Enemy(string name, float health, float damage)
+        {
+            this.Name = name;
+            this.MaxHp = health;
+            this.Damage = damage;
+            this.CurrentHp = health;
+
+        }
+
+        public override float Attack()
+        {
+            return Damage;
+        }
+
+        public override void Heal()
+        {
+            if (CurrentHp < MaxHp)
+            {
+                if (CurrentHp <= MaxHp * 0.8f)
+                {
+                    CurrentHp += MaxHp * 0.2f;
+                }
+                else
+                {
+                    CurrentHp = MaxHp;
+                }
+            }
+        }
+
+        public override void Defend()
+        {
+            isDefending = true;
+        }
+    }
+}
---
+++ SoftwareTestExamProject/Functionality/Inputs.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace SoftwareTestExamProject.Functionality
+{
+    public class Inputs
+    {
+        public string NameInput(string input)
+        {
+            string result;
+
+            /*
+            ^ : start of string
+            [ : beginning of character group
+            a-z : any lowercase letter
+            A-Z : any uppercase letter
+            ÆØÅ : allow Æ Ø Å
+            0-9 : any digit
+            _ : underscore
+            ] : end of character group
+            * : zero or more of the given characters
+            $ : end of string
+            */
+            var regexItem = new Regex("^[ÆØÅæøåa-zA-Z0-9]*$");
+
+            if (input.Length < 40 && regexItem.IsMatch(input) == true && input.Length > 0)
+            {
+                result = input;
+            }
+            else
+            {
+                result = "-1";
+            }
+
+            return result;
+        }
+
+        public int HPInput(string input)
+        {
+            int result;
+
+            if (Int32.TryParse(input, out int playerHP))
+            {
+                if (playerHP < 0 && playerHP > 1000)
+                {
+                    result = -1;
+                }
+                else
+                {
+                    result = playerHP;
+                }
+            }
+            else
+            {
+                result = -1;
+            }
+
+            return result;
+        }
+
+        public int DMGInput(string input)
+        {
+            int result;
+            if (Int32.TryParse(input, out int playerDMG))
+            {
+                if (playerDMG < 0 && playerDMG > 100)
+                {
+                    result = -1;
+                }
+                else
+                {
+                    result = playerDMG;
+                }
+            }
+            else
+            {
+                result = -1;
+            }
+            return result;
+        }
+    }
+}
