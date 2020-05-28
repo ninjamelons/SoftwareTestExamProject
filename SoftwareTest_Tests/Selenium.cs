c00be5b4a8cee8b944fd56eda0cdb9f80cacd515
Filip Olsen <filip.kenya@gmail.com>
2020-05-28 11:57:28 +0200

    selenium + selenium test class

---
+++ SoftwareTest_Tests/Selenium.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SoftwareTest_Tests
+{
+    [TestClass]
+    public class Selenium
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("www.google.com");
+        }
+    }
+}
